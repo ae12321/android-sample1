f2609fb3455a2153e1c0fd1155d7625a63023b5d
a.endo.w <80916217+ae12321@users.noreply.github.com>
2023-03-21 13:30:27 +0900

    popup

--- app/src/main/java/com/example/sampleproject/MainActivity.java
+++ app/src/main/java/com/example/sampleproject/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.sampleproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.sampleproject.popup.PopupClass;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button popupButton = findViewById(R.id.btnPopup);
+        popupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupClass popupClass = new PopupClass();
+                popupClass.showPopupWindow(view);
+            }
+        });
+
     }
 }
